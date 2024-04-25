513e4bdb21d4089df1f94971e98d2a62832a99a2
SwiftSeal <moraysmith98@gmail.com>
2024-04-25 13:54:43 +0100

    Update memory allocation in run_yahs.sh script

--- scripts/run_yahs.sh
+++ scripts/run_yahs.sh
@@ -2,12 +2,10 @@
 
 #SBATCH -p short
 #SBATCH -c 1
-#SBATCH --mem=32G
+#SBATCH --mem=8G
 #SBATCH --export=ALL
 #SBATCH -o logs/yahs.%j.out
 #SBATCH -e logs/yahs.%j.err
-
-mkdir -p results/yahs
 
 ASSEMBLY_PATH=$(realpath results/quickmerge/merged_quickmerge.fasta)
 PAIRTOOLS_BAM="results/scaffolding/mapped_pairtools.bam"
@@ -16,16 +14,16 @@ $APPS/yahs/yahs \
   --no-contig-ec \
   $ASSEMBLY_PATH \
   $PAIRTOOLS_BAM \
-  -o results/yahs/yahs.out
+  -o results/scaffolding/yahs.out
 
 $APPS/yahs/juicer pre \
   -a \
-  -o results/yahs/out_JBAT \
-  results/yahs/yahs.out.bin \
-  results/yahs/yahs.out_scaffolds_final.agp \
-  $ASSEMBLY_PATH.fai > results/yahs/out_JBAT.log 2>&1
+  -o results/scaffolding/out_JBAT \
+  results/scaffolding/yahs.out.bin \
+  results/scaffolding/yahs.out_scaffolds_final.agp \
+  $ASSEMBLY_PATH.fai > results/scaffolding/out_JBAT.log 2>&1
 
 java -jar -Xmx32G $APPS/juicer_tools.1.9.9_jcuda.0.8.jar pre \
-  results/yahs/out_JBAT.txt \
-  results/yahs/out_JBAC.hic \
-  <(cat results/yahs/out_JBAT.log | grep PRE_C_SIZE | awk '{print $2" "$3}')
+  results/scaffolding/out_JBAT.txt \
+  results/scaffolding/out_JBAC.hic \
+  <(cat results/scaffolding/out_JBAT.log | grep PRE_C_SIZE | awk '{print $2" "$3}')
