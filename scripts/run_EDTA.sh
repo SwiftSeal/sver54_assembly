198f1188da72e6a118e824d2a474118cb8266e58
SwiftSeal <moraysmith98@gmail.com>
2024-05-07 14:10:00 +0100

    update EDTA

--- scripts/run_EDTA.sh
+++ scripts/run_EDTA.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH -p short
-#SBATCH -c 64
-#SBATCH --mem=128G
+#SBATCH -p medium
+#SBATCH -c 32
+#SBATCH --mem=64G
 #SBATCH --export=ALL
 #SBATCH -o logs/edta.%j.out
 #SBATCH -e logs/edta.%j.err
 
-ASSEMBLY_FASTA="/mnt/shared/scratch/msmith/solanum_verrucosum/results/genome/solanum_verrucosum.fa"
+ASSEMBLY_FASTA="results/final_assembly/final_assembly.fa"
 
 mkdir -p results/edta
 cd $TMPDIR
@@ -15,9 +15,9 @@ cd $TMPDIR
 source activate EDTA2
 
 $APPS/EDTA/EDTA.pl --genome "$ASSEMBLY_FASTA" \
-    --threads 64 \
+    --threads 32 \
     --anno 1
 
-cd -
+cd $SLURM_SUBMIT_DIR
 
 cp -r $TMPDIR/* results/edta/
