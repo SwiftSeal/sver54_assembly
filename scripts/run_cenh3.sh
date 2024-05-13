7c434f77f60b717c14b9f6fc9d141a5baeaae3f1
SwiftSeal <moraysmith98@gmail.com>
2024-05-13 14:14:58 +0100

    update

--- scripts/run_cenh3.sh
+++ scripts/run_cenh3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -p short
-#SBATCH -c 8
+#SBATCH -c 4
 #SBATCH --mem=8G
 #SBATCH --export=ALL
 #SBATCH -o logs/cenh3.%j.out
@@ -14,7 +14,7 @@ BAMCOVERAGE="singularity exec -B /mnt/:/mnt/ docker://quay.io/biocontainers/deep
 
 mkdir -p results/cenh3
 
-$BOWTIE2_BUILD results/final_assembly/final_assembly.fa results/cenh3/final_assembly
-$BOWTIE2 -x results/cenh3/final_assembly -U data/SRR18548893.fastq.gz -S results/cenh3/aln.sam
+$BOWTIE2_BUILD results/final_assembly/final_assembly.fa $TMPDIR/final_assembly
+$BOWTIE2 -p 4 -x TMPDIR/final_assembly -U data/SRR18548893.fastq.gz -S results/cenh3/aln.sam
 $SAMTOOLS view -b -o results/cenh3/aln.bam results/cenh3/aln.sam
 $BAMCOVERAGE -b results/cenh3/aln.bam -o results/cenh3/aln.bw
