9d9491b9f096dd4539e84db2a991d96e36e39962
SwiftSeal <moraysmith98@gmail.com>
2024-05-13 14:11:53 +0100

    update

--- scripts/run_cenh3.sh
+++ scripts/run_cenh3.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 #SBATCH -p short
-#SBATCH -c 4
+#SBATCH -c 8
 #SBATCH --mem=8G
 #SBATCH --export=ALL
 #SBATCH -o logs/cenh3.%j.out
 #SBATCH -e logs/cenh3.%j.err
 
-MINIMAP2="singularity exec -B /mnt/:/mnt/ docker://quay.io/biocontainers/minimap2:2.28--he4a0461_1 minimap2"
-SAMTOOLS="singularity exec -B /mnt/:/mnt/ docker://quay.io/biocontainers/samtools:1.20--h50ea8bc_0 samtools"
-BAMCOVERAGE="singularity exec -B /mnt/:/mnt/ docker://quay.io/biocontainers/deeptools:3.5.5--pyhdfd78af_0 bamCoverage"
+BOWTIE2_BUILD="singularity exec -B /mnt/:/mnt/ docker://quay.io/biocontainers/bowtie2:2.5.3--py39h6fed5c7_1 bowtie2-build"
+BOWTIE2="singularity exec -B /mnt/:/mnt/ docker://quay.io/biocontainers/bowtie2:2.5.3--py39h6fed5c7_1 bowtie2"
 
 mkdir -p results/cenh3
 
-$MINIMAP2 -t 4 -ax sr results/final_assembly/final_assembly.fa data/SRR18548893.fastq.gz > results/cenh3/aln.sam
-
+$BOWTIE2_BUILD results/final_assembly/final_assembly.fa results/cenh3/final_assembly
+$BOWTIE2 -x results/cenh3/final_assembly -U data/SRR18548893.fastq.gz -S results/cenh3/aln.sam
 $SAMTOOLS view -b -o results/cenh3/aln.bam results/cenh3/aln.sam
 $BAMCOVERAGE -b results/cenh3/aln.bam -o results/cenh3/aln.bw
