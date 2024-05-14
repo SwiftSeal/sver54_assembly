20e8a9dde5632cbaee699f11de1a8fb2862be95b
SwiftSeal <moraysmith98@gmail.com>
2024-05-14 16:31:56 +0100

    update

--- scripts/run_remapping.sh
+++ scripts/run_remapping.sh
@@ -10,6 +10,7 @@
 # why write nice code when copy and paste exists
 
 BOWTIE2="singularity exec -B /mnt/:/mnt/ docker://quay.io/biocontainers/bowtie2:2.5.3--py39h6fed5c7_1 bowtie2"
+BOWTIE2_BUILD="singularity exec -B /mnt/:/mnt/ docker://quay.io/biocontainers/bowtie2:2.5.3--py39h6fed5c7_1 bowtie2-build"
 SAMTOOLS="singularity exec -B /mnt/:/mnt/ docker://quay.io/biocontainers/samtools:1.20--h50ea8bc_0 samtools"
 VARSCAN="singularity exec -B /mnt/:/mnt/ docker://quay.io/biocontainers/varscan:2.4.6--hdfd78af_0 varscan"
 
@@ -25,6 +26,8 @@ RENSEQ_PARENT_SUSCEPTIBLE="-1 data/ERR2222742_1.fastq.gz -2 data/ERR2222742_2.fa
 RENSEQ_BULK_RESISTANT="-1 data/ERR2222744_1.fastq.gz -2 data/ERR2222744_2.fastq.gz"
 RENSEQ_BULK_SUSCEPTIBLE="-1 data/ERR2222743_1.fastq.gz -2 data/ERR2222743_2.fastq.gz"
 
+$BOWTIE2_BUILD results/final_assembly/final_assembly.fa $TMPDIR/final_assembly
+
 bowtie2 \
   -p 4 \
   --score-min L,-0.18,-0.18 \
@@ -34,6 +37,7 @@ bowtie2 \
   --very-sensitive \
   --no-unal \
   --no-discordant \
+  -x $TMPDIR/final_assembly \
   $GENSEQ_PARENT_RESISTANT \
   > results/remapping/genseq_parent_resistant.sam
 
@@ -46,6 +50,7 @@ bowtie2 \
   --very-sensitive \
   --no-unal \
   --no-discordant \
+  -x $TMPDIR/final_assembly \
   $GENSEQ_PARENT_SUSCEPTIBLE \
   > results/remapping/genseq_parent_susceptible.sam
 
@@ -58,6 +63,7 @@ bowtie2 \
   --very-sensitive \
   --no-unal \
   --no-discordant \
+  -x $TMPDIR/final_assembly \
   $GENSEQ_BULK_RESISTANT \
   > results/remapping/genseq_bulk_resistant.sam
 
@@ -70,6 +76,7 @@ bowtie2 \
   --very-sensitive \
   --no-unal \
   --no-discordant \
+  -x $TMPDIR/final_assembly \
   $GENSEQ_BULK_SUSCEPTIBLE \
   > results/remapping/genseq_bulk_susceptible.sam
 
@@ -82,6 +89,7 @@ bowtie2 \
   --very-sensitive \
   --no-unal \
   --no-discordant \
+  -x $TMPDIR/final_assembly \
   $RENSEQ_PARENT_RESISTANT \
   > results/remapping/renseq_parent_resistant.sam
 
@@ -94,6 +102,7 @@ bowtie2 \
   --very-sensitive \
   --no-unal \
   --no-discordant \
+  -x $TMPDIR/final_assembly \
   $RENSEQ_PARENT_SUSCEPTIBLE \
   > results/remapping/renseq_parent_susceptible.sam
 
@@ -106,6 +115,7 @@ bowtie2 \
   --very-sensitive \
   --no-unal \
   --no-discordant \
+  -x $TMPDIR/final_assembly \
   $RENSEQ_BULK_RESISTANT \
   > results/remapping/renseq_bulk_resistant.sam
 
@@ -118,6 +128,7 @@ bowtie2 \
   --very-sensitive \
   --no-unal \
   --no-discordant \
+  -x $TMPDIR/final_assembly \
   $RENSEQ_BULK_SUSCEPTIBLE \
   > results/remapping/renseq_bulk_susceptible.sam
 
