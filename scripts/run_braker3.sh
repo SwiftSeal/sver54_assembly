f4723f616163d99bbbbe1138a3bad275fbd02ffc
SwiftSeal <moraysmith98@gmail.com>
2024-05-17 22:34:14 +0100

    update

--- scripts/run_braker3.sh
+++ scripts/run_braker3.sh
@@ -9,6 +9,9 @@
 
 ASSEMBLY_FILE="results/final_assembly/final_assembly.fa.new.masked"
 MERGED_BAM="results/star/merged.bam"
+AGAT_SP_EXTRACT_SEQUENCES="singularity exec -B /mnt/:/mnt/ docker://quay.io/biocontainers/agat:1.4.0--pl5321hdfd78af_0 agat_sp_extract_sequences.pl"
+AGAT_CONVERT_SP_GXF2GXF="singularity exec -B /mnt/:/mnt/ docker://quay.io/biocontainers/agat:1.4.0--pl5321hdfd78af_0 agat_convert_sp_gxf2gxf.pl"
+AGAT_CONVERT_SP_GFF2BED="singularity exec -B /mnt/:/mnt/ docker://quay.io/biocontainers/agat:1.4.0--pl5321hdfd78af_0 agat_convert_sp_gff2bed.pl"
 
 singularity exec -B /mnt/:/mnt/ $APPS/braker/braker3.sif braker.pl \
   --AUGUSTUS_CONFIG_PATH=$APPS/braker/Augustus/config/ \
@@ -18,3 +21,8 @@ singularity exec -B /mnt/:/mnt/ $APPS/braker/braker3.sif braker.pl \
   --softmasking \
   --threads 16 \
   --workingdir=results/braker
+
+$AGAT_CONVERT_SP_GXF2GXF -g results/braker/braker.gtf -o results/braker/braker.gff
+$AGAT_SP_EXTRACT_SEQUENCES -g results/braker/braker.gff -f results/final_assembly/final_assembly.fa -p -t cds -o results/braker/braker.pep.fa
+$AGAT_SP_EXTRACT_SEQUENCES -g results/braker/braker.gff -f results/final_assembly/final_assembly.fa -t cds -o results/braker/braker.cds.fa
+$AGAT_CONVERT_SP_GFF2BED --gff results/braker/braker.gff --sub gene -o results/braker/braker.bed
---
+++ scripts/run_busco.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH -p short
+#SBATCH -c 8
+#SBATCH --mem=8G
+#SBATCH --export=ALL
+#SBATCH -o logs/busco.%j.out
+#SBATCH -e logs/busco.%j.err
+
+BUSCO="singularity exec docker://ezlabgva/busco:v5.7.0_cv1 busco"
+
+$BUSCO \
+  -c 8 \
+  -i results/braker/braker.pep.fa \
+  -o braker \
+  --out_path results/busco \
+  -m prot \
+  -l viridiplantae_odb10
+
+$BUSCO \
+  -c 8 \
+  -i results/helixer/helixer.pep.fa \
+  -o helixer \
+  --out_path results/busco \
+  -m geno \
+  -l viridiplantae_odb10
