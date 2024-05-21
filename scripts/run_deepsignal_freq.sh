56244ba4fc7b3e0e5b00f7f658b9ea672943fe30
SwiftSeal <moraysmith98@gmail.com>
2024-05-21 13:56:36 +0100

    update

---
+++ scripts/run_deepsignal_freq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -p himem
+#SBATCH -c 1
+#SBATCH --mem=200gb
+#SBATCH --export=ALL
+#SBATCH -o logs/deepsignal_freq.%j.out
+#SBATCH -e logs/deepsignal_freq.%j.err
+
+cat results/deepsignal/* > $TMPDIR/mods.tsv
+
+deepsignal_plant call_freq \
+  --input_path $TMPDIR/mods.tsv \
+  --result_file results/deepsignal/freq.tsv
+
+python scripts/split_freq_file_by_5mC_motif.py --freqfile $TMPDIR/freq.tsv
